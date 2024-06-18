b052d5f4080e55902ceb7a4b18cd020c020a891d
benjamintowle04 <benjamintowle04@gmail.com>
2024-06-18 10:35:25 -0500

    Removed unnecessary path variables from Paths.py

--- utils/Paths.py
+++ utils/Paths.py
@@ -1,9 +1,6 @@
 from enum import Enum
 
 class Paths(Enum):
-    GRID_GENERATOR_THABANG = "/Users/thabang/Downloads/PullingClassSchedules/gridGenerator/gridGenerator.py"
-    GRID_GENERATOR_BEN = "C:\DSSO\PullingClassSchedules\gridGenerator\main.py"
-    PYTHON_PATH_THABANG = "/usr/local/bin/python3.10"
     SS_SCHEDULE_SHIFTS = "/2023.1/api/io/ScheduleShift"
     SS_SCHEDULE = f"/2023.1/api/io/Schedule"
     SS_LOCATIONS = f"/2023.1/api/io/Business"
